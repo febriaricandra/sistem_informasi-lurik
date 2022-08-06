f14cae8ddbf0ae2686ea6dbd87dfe33fa742a814
febriaricandra <febriaricandraproduction@gmail.com>
2022-08-06 15:49:41 +0700

    add all current working

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,10 +5,23 @@
     use Illuminate\Http\Request;
 
     class FormController extends Controller{
-        public function index(){
+        public function index(Request $request){
+            $total = 0;
+            $productsInCart = [];
+
+            $productsInSession = $request->session()->get('products');
+            if($productsInSession){
+                $productsInCart = Product::findMany(array_keys($productsInSession));
+                $total = Product::sumByQuantities($productsInCart, $productsInSession);
+            }
             $viewData = [];
             $viewData['title'] = 'Form Pembelian';
             $viewData['subtitle'] = 'Form Pembelian';
+            $viewData['products'] = $productsInCart;
+            $viewData['Total'] = $total;
             return view('form.index')->with("viewData", $viewData);
+        }
+        public function store(){
+            //asu asu
         }
     }
--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -2,5 +2,45 @@
 @section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
 @section('content')
-<h1>on progress</h1>
+<form>
+  <div class="form-group">
+    <label>Nama Penerima</label>
+    <input type="name" class="form-control" id="" placeholder="Nama">
+  </div>
+  <div class="form-group">
+    <label>Email</label>
+    <input type="text" class="form-control" id="" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label>Nomor HP Penerima</label>
+    <input type="number" class="form-control" id="" placeholder="Nomor HP">
+  </div>
+  <div class="form-group">
+    <label>Alamat Penerima</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <div class="form-group">
+    <label>Kota</label>
+    <input type="text" class="form-control" id="" placeholder="Kota">
+  </div>
+  <div class="form-group">
+    <label>Provinsi</label>
+    <input type="text" class="form-control" id="" placeholder="Provinsi">
+  </div>
+  <h1 class="bg-primary text-white m-2 rounded">
+    Detail Produk
+  </h1>
+  @foreach($viewData['products'] as $product)
+  <div class="form-group">
+    <label>Nama Produk:</label>
+    <input type="text" class="form-control" value="{{$product->getNamaProduk()}}" disabled> 
+  </div>
+  <hr />
+  @endforeach
+  <div class="form-group">
+    <label>Total:</label>
+    <input type="text" class="form-control" value="{{$viewData['Total']}}" disabled> 
+  </div>
+  <button type="submit" class="btn btn-primary mt-2">Submit</button>
+</form>
 @endsection
